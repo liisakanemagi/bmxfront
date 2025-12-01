461a11dff4f79a505035312c3559612eb8a781f0
liisakanemagi <liisa.kanemagi@gmail.com>
2025-12-01 12:06:17 +0200

    LIISA FEB-1 WIP

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <nav>
+          <nav>
+            <router-link to="/">Home</router-link> |
+          </nav>
+        </nav>
+      </div>
+      <div class="col">
+      </div>
+      <div class="col">
+        <router-link to="/login">
+        <nav>
+          <button type="button" class="btn btn-secondary btn-sm">Logi sisse / registreeri</button>
+        </nav>
+        </router-link>
+      </div>
+    </div>
+  </div>
+
   <router-view/>
 </template>
+<script setup lang="ts">
+</script>
