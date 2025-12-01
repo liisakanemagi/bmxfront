e4c8a09935086e8f46422a0b7e3018eda91e80a1
opilane <meelis.n@gmail.com>
2025-12-01 12:06:24 +0200

    FEB LOGIN WIP

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <nav>
+          <router-link to="/">Home</router-link> |
+
+        </nav>
+      </div>
+      <div class="col">
+        <nav>
+
+        </nav>
+      </div>
+      <div class="col">
+        <nav>
+          <router-link to="/login">
+        <button type="button" class="btn btn-secondary btn-sm">Logi sisse / registreeri</button>
+          </router-link>
+        </nav>
+      </div>
+    </div>
+  </div>
+
   <router-view/>
 </template>
+<script setup lang="ts">
+</script>
