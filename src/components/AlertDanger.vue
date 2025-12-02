fbc162eea7830e513b47111aab9e7d104b746594
opilane <meelis.n@gmail.com>
2025-12-02 13:26:25 +0200

    FEB LOGIN WIP 3

---
+++ src/components/AlertDanger.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <div v-if="alertMessage !== ''" class="col alert alert-danger alert-dismissible" role="alert">
+    {{ alertMessage }}
+
+    <button @click="$emit('event-alert-box-closed')"  type="button" class="btn-close"  aria-label="Close"></button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AlertDanger',
+  props: {
+    alertMessage: String
+  }
+}
+</script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="container text-center mb-3">
-    <div class="row justify-content-center mt-5">
-
+    <div class="row justify-content-center">
+      <div class="col-6">
+        <AlertDanger :alert-message="alertMessage" @event-alert-box-closed="resetAlertMessage" />
+      </div>
+    </div>
+    <div class="row justify-content-center mt-3">
       <div class="col col-2">
         <div class="mb-3">
           <div class="input-group mb-3">
@@ -24,7 +28,7 @@
       <div class="col-2">
         <!--  <button @click="executeLogin" type="button" class="btn btn-secondary btn-sm ">Logi sisse</button>
   -->
-          <button @click="executeLogin" class="btn btn-secondary btn-sm" type="button" :disabled="isFetchingData">
+          <button @click="processLogin" class="btn btn-secondary btn-sm" type="button" :disabled="isFetchingData">
             <span v-if="isFetchingData" class="spinner-border spinner-border-sm btn-sm" aria-hidden="true"></span>
             <span class="btn btn-secondary btn-sm" role="status">Logi sisse</span>
           </button>
@@ -40,9 +44,11 @@
   import {start} from "@popperjs/core";
   import errorView from "@/views/ErrorView.vue";
   import NavigationService from "@/services/NavigationService";
+  import AlertDanger from "@/components/AlertDanger.vue";
 
   export default {
     name: 'LoginView',
+    components: {AlertDanger},
     props: {},
 
     data() {
@@ -50,6 +56,7 @@
         isFetchingData: false,
         username: '',
         password: '',
+        alertMessage:'',
 
         loginResponse: {
           userId: 0,
@@ -64,31 +71,60 @@
     },
     methods: {
 
+      processLogin() {
+        if (this.username!=='' && this.password!=='') {
+          this.executeLogin();
+        } else {
+          this.displayIncorrectInputAlert();
+        }
+      },
+
+      displayIncorrectInputAlert() {
+        this.alertMessage='Täida kõik väljad'
+      },
+
+
+      executeLogin() {
+        this.startSpinner()
+        LoginService.sendGetLoginRequest(this.username, this.password)
+            .then(response => this.handleLoginResponse(response))
+            .catch(error => this.handleLoginError(error))
+            .finally(() => this.isFetchingData=false)
+
+
+
+      },
 
       handleLoginResponse(response) {
         this.loginResponse = response.data;
         sessionStorage.setItem('userId', this.loginResponse.userId)
         sessionStorage.setItem('roleName', this.loginResponse.roleName)
 
-
-      },
-
-      executeLogin() {
-        this.startSpinner()
-        LoginService.sendGetLoginRequest(this.username, this.password)
-            .then(response => this.handleLoginResponse(response))
-            .catch(error => this.errorResponse = error.response.data)
-            .finally(() => this.isFetchingData=false)
-
         NavigationService.navigateToHomeView()
-
       },
-
-
 
       startSpinner(){
         this.isFetchingData = true
       },
+
+      handleLoginError(error) {
+        this.errorResponse = error.response.data
+        if (error.response.status === 403 && this.errorResponse.errorCode === 111) {
+
+          this.alertMessage =this.errorResponse.message
+
+        }
+        else {
+            NavigationService.navigateToErrorView();
+        }
+
+
+      },
+      resetAlertMessage() {
+        this.alertMessage=''
+
+      },
+
 
 
 
