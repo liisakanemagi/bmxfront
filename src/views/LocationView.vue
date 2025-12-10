581958cbfa6a87dcb1d48f0c5f5edc15e84f9652
opilane <meelis.n@gmail.com>
2025-12-10 10:32:47 +0200

    FEB EDIT LOCATION WIP

--- src/views/LocationView.vue
+++ src/views/LocationView.vue
@@ -1,10 +1,45 @@
 <template>
   <h1>Sõidukohad</h1>
+  <div>
+  <CountyDropdown :counties="counties" @event-new-county-selected="setNewCountyId"/>
+  </div>
 </template>
 
 <script>
+import CountyDropdown from "@/components/location/CountyDropdown.vue";
+import CountyService from "@/services/CountyService";
+import NavigationService from "@/services/NavigationService";
+
 export default {
   name: 'LocationView',
+  components: {CountyDropdown},
+  data() {
+    return{
+    counties: [
+      {
+        countyId: 0,
+        countyName: '',
+        zoomLevel: 0,
+        countyLng: null,
+        countyLat: null,
+      }
+    ]
+  }
+},
 
+
+methods: {
+
+  getCounties() {
+    CountyService.sendGetCountiesRequest()
+        .then(response => this.counties = response.data)
+        .catch(() => NavigationService.navigateToErrorView())
+  }
+
+},
+  mounted() {
+  this.getLocationTypes()
+  this.getCounties()
+}
 }
 </script>
