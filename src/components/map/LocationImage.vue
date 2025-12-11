9a3f77edc3b6ed519e7607a8bec5aea5a2e5c0bc
opilane <meelis.n@gmail.com>
2025-12-11 15:34:14 +0200

    complete

--- src/components/map/LocationImage.vue
+++ src/components/map/LocationImage.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <img v-if="imageData === null || imageData === ''" src="../../assets/logo.png" class="img-thumbnail">
+    <img v-if="imageData === null || imageData === ''" src="https://www.shutterstock.com/image-vector/extreme-slogan-cartoon-cat-riding-600w-1976858315.jpg" class="img-thumbnail">
     <img v-else :src="imageData" class="img-thumbnail">
   </div>
 </template>
